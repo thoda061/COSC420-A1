6849c4e4cfef6245ed2c1b9a081b337dcc79773f
Daniel Thomson <thoda061@student.otago.ac.nz>
2018-04-24 16:34:06 +1200

    implemented text menu

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -1,83 +1,274 @@
 import java.util.*;
 import java.io.*;
+import java.text.*;
 
 public class NeuralNetwork {
 
     private static ArrayList<Unit> input = new ArrayList<>();
     private static ArrayList<Unit> hidden = new ArrayList<>();
     private static ArrayList<Unit> output = new ArrayList<>();
+    private static ArrayList<String> pFileContent = new ArrayList<>();
+    private static ArrayList<String> tFileContent = new ArrayList<>();
+    private static ArrayList<String> iFileContent = new ArrayList<>();
     private static int epoch = 0;
-    private static Double errorCriterion;
+    private static Double errorCriterion = 0.0;
 
     public static void main(String[]args) {
         Scanner sc = new Scanner(System.in);
+        int command = 1;
+        while (command != 0) {
+            System.out.println("\nCOSC420 Assignment 1\n\nMenu:\n1 initialise\n2 learn"
+                    + " (specify epochs)\n3 learn (to crietion)\n4 test\n5 show weights"
+                    + "\n6 reset\n0 quit\n");
+            command = sc.nextInt();
+            switch(command) {
+                case 1: 
+                    readInput();
+                    break;      
+                case 2: 
+                    if(errorCriterion == 0.0) {
+                        System.out.println("Must initialise first");
+                    } else {
+                        learn(0);
+                    }
+                    break;
+                case 3:
+                    if(errorCriterion == 0.0) {
+                        System.out.println("Must initialise first");
+                    } else {
+                        learn(1);
+                    }
+                    break;
+                case 4:
+                    if(errorCriterion == 0.0) {
+                        System.out.println("Must initialise first");
+                    } else {
+                        test();
+                    }
+                    break;
+                case 5:
+                    if(errorCriterion == 0.0) {
+                        System.out.println("Must initialise first");
+                    } else {
+                        displayWeights();
+                    }
+                    break;
+                case 6:
+                    if(errorCriterion == 0.0) {
+                        System.out.println("Must initialise first");
+                    } else {
+                        reset();
+                    }
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Not valid input");
+            }
+        }
+    }
+    
+    public static void reset () {
+        for(Unit i : input) {
+            i.reset(hidden);
+        }
+        for(Unit i : hidden) {
+            i.reset(output);
+        }
+    }
+    
+    public static void displayWeights () {
+        NumberFormat f = new DecimalFormat("#0.0000000");
+        
+        System.out.println("\nInput to Hidden Weights\n");
+        for(int i = 0; i < hidden.size(); i++) {
+            System.out.print("\t   Hidden" + (i+1));
+        }
+        for(int i = 0; i < input.size(); i++) {
+            HashMap<Unit, Double> weights = input.get(i).getConnections();
+            System.out.print("\nInput" + (i+1));
+            for(int j = 0; j < hidden.size(); j++) {
+                System.out.print("\t" + f.format(weights.get(hidden.get(j))));
+            }
+        }
+        
+        System.out.println("\n\nHidden to Output Weights\n");
+        for(int i = 0; i < output.size(); i++) {
+            System.out.print("\t   Output" + (i+1));
+        }
+        for(int i = 0; i < hidden.size(); i++) {
+            HashMap<Unit, Double> weights = hidden.get(i).getConnections();
+            System.out.print("\nHidden" + (i+1));
+            for(int j = 0; j < output.size(); j++) {
+                System.out.print("\t" + f.format(weights.get(output.get(j))));
+            }
+        }
+        System.out.println("");
+    }
+    
+    public static void test () {
+        for(int i = 0; i < iFileContent.size(); i++) {
+            System.out.println("Input: " + iFileContent.get(i));
+            System.out.println("Expected Output: "+tFileContent.get(i)+"\n");
+            setCycle(iFileContent.get(i), tFileContent.get(i));
+            testCycle();
+        }
+    }
+    
+    public static void testCycle () {
+        for(int i = 0; i < input.size(); i++) {
+            System.out.println("Input" + i + ": " + input.get(i).getOutput());
+            input.get(i).forwardPropergate();
+        }
+        for(int i = 0; i < hidden.size(); i++) {
+            hidden.get(i).activation();
+            System.out.println("Hidden" + i + ": " + hidden.get(i).getOutput());
+            hidden.get(i).forwardPropergate();
+        }
+        for(int i = 0; i < output.size(); i++) {
+            output.get(i).activation();
+            if(output.get(i).getOutput() > 0.5) {
+                System.out.println("Output" + i + ": 1.0");
+            } else {
+                System.out.println("Output" + i + ": 0.0");
+            }
+        }
+        System.out.println("");
+    }
+    
+    public static void learn (int stopType) {
+        epoch = 0;
+        Scanner sc = new Scanner(System.in);
+        int epochLimit;
+        Double currErrorCrit;
+        if (stopType == 0) {
+            System.out.println("Enter the amount of epochs");
+            epochLimit = sc.nextInt();
+            currErrorCrit = 0.0;
+        } else {
+            epochLimit = 10000;
+            currErrorCrit = errorCriterion;
+        }
+        
+        Double popError = 1.0;
+        Double patternError = 0.0;
+        while (popError > currErrorCrit && epoch < epochLimit) {
+            popError = 0.0;
+            patternError = 0.0;
+            for(int i = 0; i < iFileContent.size(); i++) {
+                setCycle(iFileContent.get(i), tFileContent.get(i));
+                cycle();
+                for(Unit ix : output) {
+                    patternError += Math.pow(ix.getError(), 2);
+                }
+                //System.out.println("Pattern error: " + patternError);
+                popError += patternError * 0.5;
+            }
+            popError /= Double.valueOf(pFileContent.get(2))*iFileContent.size();
+            epoch++;
+            if(epoch % 100 == 0) {
+                System.out.println("Epoch " + epoch);
+                System.out.println("Population Error: " + popError);
+            }
+            for (Unit i : hidden) {
+                i.adjustWeights();
+                i.setprvWeightChange();
+                i.resetNetChange();
+            }
+            for(Unit i : input) {
+                i.adjustWeights();
+                i.setprvWeightChange();
+                i.resetNetChange();
+            }
+        }
+        System.out.println("Epoch " + epoch);
+        System.out.println("Population Error: " + popError);
+    }
+    
+    public static void readInput () {
+        Scanner sc = new Scanner(System.in);
         String line = null;
         
-        System.out.println("Parameter file name");
-        String pFile = sc.nextLine();
-        ArrayList<String> pFileContent = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(pFile));
-            while((line = br.readLine()) != null) {
-                pFileContent.add(line);
+        boolean validPFile = false;
+        while(!validPFile) {
+            System.out.println("Parameter file name");
+            String pFile = sc.nextLine();
+            pFileContent = new ArrayList<>();
+            try {
+                validPFile = true;
+                BufferedReader br = new BufferedReader(new FileReader(pFile));
+                while((line = br.readLine()) != null) {
+                    pFileContent.add(line);
+                }
+            } catch(FileNotFoundException ex) {
+                validPFile = false;
+                System.out.println(pFile + " not found");
+            } catch (IOException ex) {
+                validPFile = false;
+                ex.printStackTrace();
             }
-        } catch(FileNotFoundException ex) {
-            System.out.println(pFile + " not found");
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
+        output = new ArrayList<>();
+        hidden = new ArrayList<>();
+        input = new ArrayList<>();
         for(int i = 0; i < Integer.parseInt(pFileContent.get(2)); i++) {
             output.add(new Unit());
         }
         for(int i = 0; i < Integer.parseInt(pFileContent.get(1)); i++) {
-            hidden.add(new Unit(output, Double.valueOf(pFileContent.get(3))));
+            hidden.add(new Unit(output, Double.valueOf(pFileContent.get(3)), 
+                    Double.valueOf(pFileContent.get(4))));
         }
         for(int i = 0; i < Integer.parseInt(pFileContent.get(0)); i++) {
-            input.add(new Unit(hidden, Double.valueOf(pFileContent.get(3))));
+            input.add(new Unit(hidden, Double.valueOf(pFileContent.get(3)), 
+                    Double.valueOf(pFileContent.get(4))));
         }
         errorCriterion = Double.valueOf(pFileContent.get(5));
         
-        System.out.println("Input file name");
-        String iFile = sc.nextLine();
-        ArrayList<String> iFileContent = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(iFile));
-            while((line = br.readLine()) != null) {
-                iFileContent.add(line);
+        boolean validIFile = false;
+        while(!validIFile) {
+            System.out.println("Input file name");
+            String iFile = sc.nextLine();
+            iFileContent = new ArrayList<>();
+            try {
+                validIFile = true;
+                BufferedReader br = new BufferedReader(new FileReader(iFile));
+                while((line = br.readLine()) != null) {
+                    iFileContent.add(line);
+                }
+            } catch (FileNotFoundException ex) {
+                validIFile = false;
+                System.out.println(iFile + " not found");
+            } catch (IOException ex) {
+                validIFile = false;
+                ex.printStackTrace();
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println(iFile + " not found");
-        } catch (IOException ex) {
-            ex.printStackTrace();
         }
 
-        System.out.println("Teacher file name");
-        String tFile = sc.nextLine();
-        ArrayList<String> tFileContent = new ArrayList<>();
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(tFile));
-            while((line = br.readLine()) != null) {
-                tFileContent.add(line);
+        boolean validTFile = false;
+        while(!validTFile) {
+            System.out.println("Teacher file name");
+            String tFile = sc.nextLine();
+            tFileContent = new ArrayList<>();
+            try {
+                validTFile = true;
+                BufferedReader br = new BufferedReader(new FileReader(tFile));
+                while((line = br.readLine()) != null) {
+                    tFileContent.add(line);
+                }
+            } catch (FileNotFoundException ex) {
+                validTFile = false;
+                System.out.println(tFile + "not found");
+            } catch (IOException ex) {
+                validTFile = false;
+                ex.printStackTrace();
             }
-        } catch (FileNotFoundException ex) {
-            System.out.println(tFile + "not found");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-
-        while (epoch < 100) {
-            for(int i = 0; i < iFileContent.size(); i++) {
-                setCycle(iFileContent.get(i), tFileContent.get(i));
-                cycle();
-            }
-            epoch++;
-            System.out.println(epoch);
         }
     }
 
     public static void setCycle(String inputString, String expectedOutput) {
-        String[] splitInput = inputString.split(" ");
+        String[] splitInput = inputString.split(" +");
         for(int i = 0; i < splitInput.length; i++) {
+            //System.out.print("Input " + i + " ");
             input.get(i).setOutput(Double.valueOf(splitInput[i]));
         }
         String[] splitOutput = expectedOutput.split(" ");
@@ -93,23 +284,25 @@ public class NeuralNetwork {
 
     public static void cycle() {
         for(Unit i : input) {
+            //System.out.println("Input " + i);
             i.forwardPropergate();
         }
         for(Unit i : hidden) {
+            //System.out.println("Hidden " + i);
             i.activation();
             i.forwardPropergate();
         }
         for(Unit i : output) {
-            i.activation();
+            i.outputActivation();
             i.setError();
-            System.out.println(i.getError());
+            //System.out.println("Output " + i.getOutput());
         }
         for(Unit i : hidden) {
+            i.addWeightChange();
             i.setErrorHidden();
-            i.adjustWeights();
         }
         for(Unit i : input) {
-            i.adjustWeights();
+            i.addWeightChange();
         }
     }
 }
--- Unit.java
+++ Unit.java
@@ -1,5 +1,4 @@
 import java.util.*;
-import java.lang.Math.*;
 
 public class Unit {
 
@@ -9,6 +8,9 @@ public class Unit {
     private Double expectedOut;
     private Double error;
     private Double learningConstant;
+    private Double momentum;
+    private HashMap<Unit, Double> prvWeightChange = new HashMap<>();
+    private HashMap<Unit, Double> netChanges = new HashMap<>();
 
     public Unit() {
         error = 0.0;
@@ -16,21 +18,41 @@ public class Unit {
         input = 0.0;
         output = 0.0;
         expectedOut = 0.0;
+        momentum = 0.0;
     }
 
-    public Unit (ArrayList<Unit> nextLayer, Double learningConstant) {
+    public Unit (ArrayList<Unit> nextLayer, Double learningConstant, Double momentum) {
+        Random rand = new Random();
         for (Unit i : nextLayer) {
-            connectionWeights.put(i,0.0);
+            connectionWeights.put(i,rand.nextDouble() - 0.5);
+            prvWeightChange.put(i, 0.0);
         }
         error = 0.0;
         expectedOut = 0.0;
         input = 0.0;
         output = 0.0;
         this.learningConstant = learningConstant;
+        this.momentum = momentum;
+        
+    }
+    
+    public HashMap<Unit, Double> getConnections() {
+        return connectionWeights;
     }
 
     public void setExpectedOut (Double out) {
         expectedOut = out;
+    }
+    
+    public void reset (ArrayList<Unit> nextLayer) {
+        connectionWeights = new HashMap<>();
+        prvWeightChange = new HashMap<>();
+        
+        Random rand = new Random();
+        for (Unit i : nextLayer) {
+            connectionWeights.put(i,rand.nextDouble() - 0.5);
+            prvWeightChange.put(i, 0.0);
+        }
     }
 
     public void setInput (Double in) {
@@ -39,24 +61,49 @@ public class Unit {
 
     public void setOutput (Double out) {
         output = out;
+        //System.out.println(out);
     }
 
     public void addInput (Double in) {
         input += in;
     }
+    
+    public Double getInput() {
+        return input;
+    }
+    
+    public Double getOutput() {
+        return output;
+    }
 
     public void forwardPropergate () {
         for (Unit i : connectionWeights.keySet()) {
+            //System.out.println("Weight " + i + ": " + connectionWeights.get(i));
             i.addInput(connectionWeights.get(i)*output);
+            //System.out.println("In: " + getInput());
         }
     }
 
     public void activation () {
         output = 1.0/(1.0 + Math.exp(-1*input));
     }
+    
+    public void outputActivation () {
+        if(input >= 0.5) {
+            output = 1.0;            
+        } else {
+            output = 0.0;
+        }
+    }
+    
+    public void resetError() {
+        error = 0.0;
+    }
 
     public void setError () {
+        //System.out.println("In " + getInput());
         error = (expectedOut - output) * output * (1 - output);
+        //System.out.println("Error " + error);
     }
 
     public void setErrorHidden () {
@@ -70,12 +117,39 @@ public class Unit {
     public Double getError () {
         return error;
     }
-
+    
+    public Double getExpectedOut() {
+        return expectedOut;
+    }
+    
+    public void resetNetChange() {
+        netChanges = new HashMap<>();
+    }
+    
     public void adjustWeights () {
         for(Unit i : connectionWeights.keySet()) {
+            connectionWeights.replace(i, connectionWeights.get(i) + 
+                    (netChanges.get(i) + (momentum * prvWeightChange.get(i))));
+            //System.out.println("Change: " + netChanges.get(i));
+        }
+    }
+    
+    public void setprvWeightChange () {
+        for(Unit i : connectionWeights.keySet()) {
+            prvWeightChange.replace(i, netChanges.get(i) + 
+                    (momentum * prvWeightChange.get(i)));
+        }
+    }
+
+    public void addWeightChange () {
+        for(Unit i : connectionWeights.keySet()) {
             Double change = learningConstant * i.getError() * output;
-            Double newWeight = connectionWeights.get(i) + change;
-            connectionWeights.replace(i, newWeight);
+            if(netChanges.containsKey(i)) {
+                Double newChange = netChanges.get(i) + change;
+                netChanges.replace(i, newChange);
+            } else {
+                netChanges.put(i, change);
+            }
         }
     }
 }
